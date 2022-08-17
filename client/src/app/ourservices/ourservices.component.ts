67402ed7318def97b0aa608ad52c822518a8dc93
ChrisMuteb <ibond.chris@gmail.com>
2022-08-17 13:54:01 +0300

    adding a ssl for data sent over the internet

--- client/src/app/ourservices/ourservices.component.ts
+++ client/src/app/ourservices/ourservices.component.ts
@@ -12,4 +12,31 @@ export class OurservicesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  services = [
+    {
+      'id': 1,
+      'name': 'Dental Checkup',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'image': '../../assets/dental-check-up.jpeg'
+    },
+    {
+      'id': 2,
+      'name': 'Dental Implants',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'image': '../../assets/dental implants.jpg'
+    },
+    {
+      'id': 3,
+      'name': 'Dentures',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'image': '../../assets/partial dentures.jpg'
+    },
+    {
+      'id': 4,
+      'name': 'Teeth Whitening',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'image': '../../assets/dental whitening.jpg'
+    }
+  ]
+
 }
