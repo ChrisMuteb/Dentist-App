2529ffd4a80a2e53a94a11d6384aa0a92e150abd
ChrisMuteb <ibond.chris@gmail.com>
2022-08-17 09:22:51 +0300

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewpatientComponent } from './newpatient/newpatient.component';
+import { OurservicesComponent } from './ourservices/ourservices.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BookanappointmentComponent } from './bookanappointment/bookanappointment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NewpatientComponent,
+    OurservicesComponent,
+    ContactusComponent,
+    AboutusComponent,
+    BookanappointmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
