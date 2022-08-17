d5203b46456b32e8e82eb0154369699bb6e25491
ChrisMuteb <ibond.chris@gmail.com>
2022-08-17 09:51:39 +0300

    Nav-bar added and each nav-tem is linked to its component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BookanappointmentComponent } from './bookanappointment/bookanappointment.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { NewpatientComponent } from './newpatient/newpatient.component';
+import { OurservicesComponent } from './ourservices/ourservices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'newpatient', component: NewpatientComponent},
+  {path: 'ourservice', component: OurservicesComponent},
+  {path: 'contactus', component: ContactusComponent},
+  {path: 'about', component: AboutusComponent},
+  {path: 'bookanappointment', component: BookanappointmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
